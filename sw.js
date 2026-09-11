e5909d0b32e6e5b496211bff8abc46aaa190cbb3
yoyoping60-cmd <yoyoping60@gmail.com>
2026-09-11 18:50:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "minuteur-tt-v1";
+const CACHE = "minuteur-tt-v2";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
@@ -17,8 +17,16 @@ self.addEventListener("activate", (event) => {
   self.clients.claim();
 });
 
+// Réseau en priorité (pour toujours avoir la dernière version quand il y a du signal),
+// et repli sur le cache local si hors connexion.
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
